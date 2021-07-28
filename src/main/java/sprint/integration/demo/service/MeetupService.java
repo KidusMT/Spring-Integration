b60ec0b0ffdff882c4fb66ac8a05141a4ab55483
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-07-28 01:08:32 -0500

    updated service and JmsSender files

--- src/main/java/sprint/integration/demo/service/MeetupService.java
+++ src/main/java/sprint/integration/demo/service/MeetupService.java
@@ -1,27 +1,8 @@
 package sprint.integration.demo.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import java.awt.print.Book;
+@Service
+class MeetupService {
 
-@Component
-class JmsSender {
-    @Autowired
-    JmsTemplate jmsTemplate;
-
-    public void sendMessage(Book contact)  {
-        try {
-            //convert book to JSON string
-            ObjectMapper objectMapper = new ObjectMapper();
-            String bookString = objectMapper.writeValueAsString(contact);
-            System.out.println("Sending a JMS message:" + bookString);
-            jmsTemplate.convertAndSend("testQueue", bookString);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-    }
 }
