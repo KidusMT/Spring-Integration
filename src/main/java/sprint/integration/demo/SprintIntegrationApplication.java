3c69a944b1beaf8981440fce64e560bfa6115743
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-07-27 22:31:14 -0500

    created project with spring initializr

---
+++ src/main/java/sprint/integration/demo/SprintIntegrationApplication.java
@@ -0,0 +1,13 @@
+package sprint.integration.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sprint/integration/demo/SprintIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package sprint.integration.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
